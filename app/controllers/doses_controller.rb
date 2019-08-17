8c1666d85ee9a3fccfd8bb3d4ff80648c6b0984c
Ali Kalkanel <kally-95@hotmail.com>
2019-08-17 22:13:15 +0100

    Initial commit

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -19,7 +19,7 @@ class DosesController < ApplicationController
     @dose = Dose.find(params[:id])
     # @cocktail = Cocktail.find(params[:cocktail_id])
     @dose.destroy
-    redirect_to cocktail_path(@cocktail)
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,5 +14,7 @@
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import { loadDynamicBannerText } from '../components/banner';
+loadDynamicBannerText();
 
 console.log('Hello World from Webpacker')
